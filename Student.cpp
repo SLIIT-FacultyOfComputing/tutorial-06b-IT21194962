24d1aef6a930097c1a29c6485d57e588bdf56058
Sachintha Hasaranga IT21194962 <99173290+IT21194962@users.noreply.github.com>
2022-03-14 08:55:24 +0000

    student c program

--- Student.cpp
+++ Student.cpp
@@ -1,12 +1,28 @@
 #include "Student.h"
 #include <iostream>
+#include<cstring>
+using namespace std;
 
-// Assign studentId and name
+void Student::assignDetail(int id, char pName[]){
+
+studentId=id;
+  strcpy(name,pName)
+
+  
+}
+void student::display(){
+
+  cout<<"student numberis : "<<studentId<<endl;
+  cout<<"student name is : "<<name<<endl;
+}
+
+/* Assign studentId and name
 Student::assignDetails() {
   
 }
 
-// Display StudentId and Name
+ Display StudentId and Name
 Student::display() {
   
 }
+*/
